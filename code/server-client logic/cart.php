6aa1b1bc0132345829e0e36478414d14a8eaabf9
el-patron <collins_kuria@outlook.com>
2020-03-23 01:49:31 +0300

    added AJAX to cart and split the files into connection style & select

--- code/server-client logic/cart.php
+++ code/server-client logic/cart.php
@@ -1,62 +1,33 @@
 <!DOCTYPE html>
 <html>
 <head>
-<style>
-table {
-  font-family: arial, sans-serif;
-  border-collapse: collapse;
-  width: 100%;
-}
+<link rel="stylesheet" type="text/css" href="style.css">
+<script src="JQuery/jquery-3.4.1dev.js" > </script>
 
-td, th {
-  border: 1px solid #cccccc;
-  text-align: left;
-  padding: 8px;
-}
-
-tr:nth-child(even) {
-  background-color: #dddddd;
-}
-</style>
-</head>
 <body>
 
-<h2>Scanned Goods</h2>
+  <h2>Scanned Goods</h2>
 
-<?php
+  <div id="items"></div>
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "store";
+<script type="text/javascript">
+  function display()
+  {
+    xmlhttp=new XMLHttpRequest();
+    xmlhttp.open("GET","select.php",false);
+    xmlhttp.send(null);
+    document.getElementById('items').innerHTML=xmlhttp.responseText;
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+  }
 
-$sql = "SELECT barcode,p_name,price FROM cart ";
-$result = $conn->query($sql);
-    echo "<table>
-  <tr>
-    <th>Item</th>
-    <th>Description</th>
-    <th>Price</th>
-  </tr>";
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "<tr><td>" . $row["barcode"]. "</td><td>" . $row["p_name"]. "</td><td> " . $row["price"]. "</td></tr>";
-    }
-    echo "</table>";
-//  else {
-    // echo "0 results";
-// }
+display();
 
+setInterval(
+  function(){display();},2000
+);
 
-?>
-
+</script>
+</head>
 
 
 
